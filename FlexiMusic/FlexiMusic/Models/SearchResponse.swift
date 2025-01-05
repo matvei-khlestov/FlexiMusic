92f512d8e467b89792847eab8a8f7a3f2fa69077
Matvei <khlestov.matvei@mail.ru>
2025-01-05 16:48:21 +0300

    feat: added data model for project

---
+++ FlexiMusic/FlexiMusic/Models/SearchResponse.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResponse.swift
+//  FlexiMusic
+//
+//  Created by Matvei Khlestov on 05.01.2025.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    let resultCount: Int
+    let results: [Track]
+}
+
+struct Track: Decodable {
+    let trackName: String?
+    let artistName: String?
+    let collectionName: String?
+    let artworkUrl100: URL?
+}
