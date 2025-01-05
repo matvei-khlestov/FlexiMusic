654e56796287466c637052f4129bc5f9ba059078
Matvei <khlestov.matvei@mail.ru>
2025-01-05 17:22:50 +0300

    feat: configured SearchTableViewController

--- FlexiMusic/FlexiMusic/Controllers/SearchTableViewController.swift
+++ FlexiMusic/FlexiMusic/Controllers/SearchTableViewController.swift
@@ -6,30 +6,92 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class SearchTableViewController: UITableViewController {
-
+    
+    private var tracks = [Track]()
+    private var searchTimer: Timer?
+    private let networkService = NetworkService.shared
+    private let searchController = UISearchController(
+        searchResultsController: nil
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerTableViewCell()
+        setupSearchBar()
     }
+}
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+// MARK: - Private Methods
+extension SearchTableViewController {
+    private func registerTableViewCell() {
+        tableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: "cell"
+        )
     }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    private func fetchTracks(searchTerm: String) {
+        networkService.fetchTracks(searchTerm: searchTerm) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let tracks):
+                self.tracks = tracks
+            case .failure(let error):
+                print("Error in fetch tracks: \(error)")
+            }
+            self.tableView.reloadData()
+        }
+    }
+}
 
+// MARK: - UITableViewDataSource
+extension SearchTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        tracks.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let track = tracks[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = track.trackName
+        content.secondaryText = track.artistName
+        if let url = track.artworkUrl100 {
+            let imageView = UIImageView()
+            imageView.kf.setImage(with: url)
+            content.image = imageView.image
+        }
+        
+        cell.contentConfiguration = content
         return cell
     }
-    */
+}
+
+// MARK: - UISearchBarDelegate
+extension SearchTableViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let trimmedText = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !trimmedText.isEmpty else {
+            self.tracks = []
+            self.tableView.reloadData()
+            return
+        }
+        
+        searchTimer?.invalidate()
+        searchTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
+            self.fetchTracks(searchTerm: trimmedText)
+        })
+    }
 }
